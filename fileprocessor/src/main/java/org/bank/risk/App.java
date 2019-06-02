a93243c6e9dabf15d2725974c5000cb497b4d311
parth <parth.techblogs@gmail.com>
2019-06-02 11:42:20 +0200

    add logging

--- fileprocessor/src/main/java/org/bank/risk/App.java
+++ fileprocessor/src/main/java/org/bank/risk/App.java
@@ -1,13 +1,21 @@
 package org.bank.risk;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class App {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+    public static void main( String[] args ) {
+       // MDC.put("logFileName","FileProcessor");
+        LOGGER.info( "FileProcessing" );
+        List<Integer> ints = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+        for(int i : ints){
+            LOGGER.info(String.valueOf(i));
+        }
     }
 }
